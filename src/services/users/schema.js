ee413125bbe599a99cf860279bf2e118acc22f4a
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-02-08 15:08:52 +0000

    Adding auth

--- src/services/users/schema.js
+++ src/services/users/schema.js
@@ -2,16 +2,41 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
+const UserSchema = new Schema(
+  {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     dateOfBirth: { type: Date, required: true },
-    age: { type: Number, required: true },
+    role: { type: String, enum: ["User", "Admin"], default: "User" },
     topics_covered: [String],
-},
-    {
-        timestamps: true,
-    })
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default model("User", userSchema)
+UserSchema.pre("save", async function (next) {
+    // before saving the user in the database, hash the password and
+    const newUser = this
+    const plainPW = newUser.password
+
+    if (newUser.isModified("password")) {
+        const hash = await bcrpyt.hash(plainPW, 10)
+        newUser.password = hash
+    }
+
+    next()
+})
+
+UserSchema.methods.toJSON = function () {
+    // called automatically every time express sends the users response res.send(users)
+    const userDocument = this
+    const userObject = userDocument.toObject()
+    delete userObject.password
+    delete userObject.__v
+
+    return userObject
+}
+
+export default model("User", UserSchema);
